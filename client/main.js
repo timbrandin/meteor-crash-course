92de6f40af4135bf2af2a7f529577a918f5a9b88
Tim Brandin <tim.brandin@gmail.com>
2014-08-06 15:26:27 +0200

    Step4

--- client/main.js
+++ client/main.js
@@ -1,5 +1,19 @@
 Template.messages.helpers({
   messages: function() {
     return Messages.find();
+  },
+
+  username: function() {
+    var user = Meteor.users.findOne({_id: this.user});
+    if (user) {
+      return user.username;
+    }
+  },
+
+  picture: function() {
+    var user = Meteor.users.findOne({_id: this.user});
+    if (user && user.emails) {
+      return Gravatar.imageUrl(user.emails[0].address);
+    }
   }
 });
